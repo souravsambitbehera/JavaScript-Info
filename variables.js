7ee136ec7f8de57547119e95fdcc483848736cfd
Sourav Sambit Behera <souravsambit12@gmail.com>
2022-12-27 02:15:21 +0530

    first commit

---
+++ variables.js
@@ -0,0 +1,15 @@
+// alert("sourav")
+
+// Working with variables
+
+let admin;
+let name;
+admin="Jhon";
+name=admin;
+alert(admin)
+
+// Giving the right name
+const ourPlanetName = "Earth";
+// by using const
+
+let currentUserName = "sourav";
